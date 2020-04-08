3e7a14adbc70ca1633365756ecd39cc2551ade37
jojotov <jojodingtz@gmail.com>
2020-04-08 21:11:25 +0800

    Initial Commit

---
+++ TDMakeAttributed/TDMakeAttributed.h
@@ -0,0 +1,19 @@
+//
+//  TDMakeAttributed.h
+//  TDMakeAttributed
+//
+//  Created by jojo on 2020/4/8.
+//  Copyright © 2020 jojotov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TDMakeAttributed.
+FOUNDATION_EXPORT double TDMakeAttributedVersionNumber;
+
+//! Project version string for TDMakeAttributed.
+FOUNDATION_EXPORT const unsigned char TDMakeAttributedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDMakeAttributed/PublicHeader.h>
+
+
